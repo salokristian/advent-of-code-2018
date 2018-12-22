876ebfd14c59212e3835c85a0a41afa5e2815dce
Kristian Salo <salo.kristian@hotmail.com>
2018-12-22 23:40:47 +0200

    Add spyscope for debugging

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:resource-paths ["src/advent_of_code/resources"]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [spyscope "0.1.6"]
+                 [clojure.java-time "0.3.2"]]
+  :profiles {:dev  {:resource-paths ["src/advent_of_code/resources"]}
+             :user {:dependencies [[spyscope "0.1.6"]]
+                    :injections   [(require 'spyscope.core)]}})
