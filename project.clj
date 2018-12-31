7c1dc7fccf6ca7ca6e16b29ca54dbc8d4f913e1f
Kristian Salo <salo.kristian@hotmail.com>
2018-12-31 12:12:12 +0200

    Update Clojure to 1.10

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [spyscope "0.1.6"]
                  [clojure.java-time "0.3.2"]]
   :profiles {:dev  {:resource-paths ["src/advent_of_code/resources"]}
